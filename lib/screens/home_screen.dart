ccfe78e29df5452d654b70f10a45d500ec7b9848
Grecia Osorio <you@example.com>
2024-10-15 14:23:55 -0500

    UI for User Profile tab has been added

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -6,6 +6,8 @@ import 'package:app_swe2024/models/authorization.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:app_swe2024/screens/menu_screen.dart';
+import 'package:app_swe2024/screens/account_screen.dart';
+
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -56,20 +58,77 @@ class _HomeScreenState extends State<HomeScreen> {
             );
           },
         ),
-        actions: [
-          IconButton(
-            tooltip: 'User Profile',
-            icon: const Icon(Icons.account_circle_rounded,
-                color: Color(0xFFD0EDF2), size: 30.0),
-            onPressed: () {
-              // Define the action when the user icon is pressed
+        actions: <Widget> [
+          Builder (
+            builder: (BuildContext context) {
+              return PopupMenuButton<int>(
+                color: const Color(0xFF019c9f),
+                tooltip: 'User Profile',
+                icon: const Icon(Icons.account_circle_rounded,
+                    color: Color(0xFFD0EDF2), size: 30.0),
+                onSelected: (int value) {
+                  //_handleMenuAction(value, context);
+                },
+                itemBuilder: (BuildContext context) => [
+                  const PopupMenuItem(
+                    value: 1,
+                    child: Row(
+                      children: [
+                        Padding(
+                          padding: EdgeInsets.only(right: 8.0),
+                          child: Icon(Icons.account_circle_rounded,
+                          color: Colors.black),
+                        ),
+                        Text(
+                          'My Account',
+                          style: TextStyle(fontSize: 15),
+                        ),
+                      ],
+                    ),
+                  ),
+                  const PopupMenuItem(
+                    value: 2,
+                    child: Row(
+                      children: [
+                        Padding(
+                          padding: EdgeInsets.only(right: 8.0),
+                          child: Icon(Icons.settings,
+                          color: Colors.black)
+                        ),
+                        Text(
+                          'Settings',
+                          style: TextStyle(fontSize: 15),
+                        ),
+                      ],
+                    ),
+                  ),
+                  const PopupMenuItem(
+                    value: 3, //value will add functionality, will have to be extracted to account_screen and use onSelect
+                    child: Row(
+                      children: [
+                        Padding(
+                          padding: EdgeInsets.only(right: 8.0),
+                          child: Icon(Icons.logout,
+                          color: Colors.black)
+                        ),
+                        Text(
+                          'Logout',
+                          style: TextStyle(fontSize: 15),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              );
             },
           ),
+          
         ],
       ),
       drawer: const Drawer(
         child: MenuScreen(),
       ),
+    
       body: Column(
         children: [
           Container(
